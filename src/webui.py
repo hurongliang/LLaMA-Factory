1b5827b92637259675a436866e0d5acb8f5c52f1
hurongliang <me@hurongliang.com>
2024-07-01 10:12:32 +0800

    readme

--- src/webui.py
+++ src/webui.py
@@ -21,7 +21,7 @@ def main():
     gradio_share = os.environ.get("GRADIO_SHARE", "0").lower() in ["true", "1"]
     server_name = os.environ.get("GRADIO_SERVER_NAME", "0.0.0.0")
     server_port = os.environ.get("GRADIO_SERVER_PORT", "8080")
-    create_ui().queue().launch(share=gradio_share, server_name=server_name, server_port=server_port, inbrowser=True)
+    create_ui().queue().launch(share=gradio_share, server_name=server_name, server_port=int(server_port), inbrowser=True)
 
 
 if __name__ == "__main__":
