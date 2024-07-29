da805314a7227acc1b541a21e5657f2f5125d61c
hurongliang <me@hurongliang.com>
2024-07-29 23:18:38 +0800

    readme

--- src/webui.py
+++ src/webui.py
@@ -20,7 +20,7 @@ from llamafactory.webui.interface import create_ui
 def main():
     gradio_share = os.environ.get("GRADIO_SHARE", "0").lower() in ["true", "1"]
     server_name = os.environ.get("GRADIO_SERVER_NAME", "0.0.0.0")
-    server_port = os.environ.get("GRADIO_SERVER_PORT", "7860")
+    server_port = os.environ.get("GRADIO_SERVER_PORT", "6006")
     create_ui().queue().launch(share=gradio_share, server_name=server_name, server_port=int(server_port), inbrowser=False)
 
 
